5a7d79471c22ccd341daed296ae862054c9c11cc
ReddyArunreddy <arunreddyfulla@gmail.com>
2018-12-28 15:32:24 +0530

    Grow TxQ expected size quickly, shrink slowly:
    * Stores recent history of "good" ledgers. Uses the maximum as the
      expected ledger size. When a large value drops off, use a 90%
      backoff to go down to to the new maximum.
    * If consensus is unhealthy, wipe the history in addition to the current
      clamping.
    * Include .md doc files in xcode and VS projects

--- src/casinocoin/app/misc/TxQ.h
+++ src/casinocoin/app/misc/TxQ.h
@@ -32,7 +32,7 @@
 #include <casinocoin/protocol/TER.h>
 #include <casinocoin/protocol/STTx.h>
 #include <boost/intrusive/set.hpp>
-
+#include <boost/circular_buffer.hpp>
 namespace casinocoin {
 
 class Application;
@@ -200,6 +200,9 @@ private:
         // One more than this value will be accepted
         // before escalation kicks in.
         std::size_t txnsExpected_;
+        // Recent history of transaction counts that
+        // exceed the targetTxnCount_
+        boost::circular_buffer<std::size_t> recentTxnCounts_;
         // Minimum value of escalationMultiplier.
         std::uint64_t const minimumMultiplier_;
         // Based on the median fee of the LCL. Used
@@ -219,6 +222,7 @@ private:
                     targetTxnCount_ : *setup.maximumTxnInLedger :
                         boost::optional<std::size_t>(boost::none))
             , txnsExpected_(minimumTxnCount_)
+            , recentTxnCounts_(setup.ledgersInQueue)
             , minimumMultiplier_(setup.minimumEscalationMultiplier)
             , escalationMultiplier_(minimumMultiplier_)
             , j_(j)
--- src/casinocoin/app/misc/impl/TxQ.cpp
+++ src/casinocoin/app/misc/impl/TxQ.cpp
@@ -35,6 +35,7 @@
 #include <boost/algorithm/clamp.hpp>
 #include <limits>
 #include <numeric>
+#include <algorithm>
 
 namespace casinocoin {
 
@@ -113,16 +114,32 @@ TxQ::FeeMetrics::update(Application& app,
         // so clamp down on limits.
         txnsExpected_ = boost::algorithm::clamp(feeLevels.size(),
             minimumTxnCount_, targetTxnCount_);
+        recentTxnCounts_.clear();
     }
     else if (feeLevels.size() > txnsExpected_ ||
         feeLevels.size() > targetTxnCount_)
     {
+        recentTxnCounts_.push_back(feeLevels.size());
+        auto const iter = std::max_element(recentTxnCounts_.begin(),
+            recentTxnCounts_.end());
+        BOOST_ASSERT(iter != recentTxnCounts_.end());
+        auto const next = [&]
+        {
+            // Grow quickly: If the max_element is >= the
+            // current size limit, use it.
+            if (*iter >= txnsExpected_)
+                return *iter;
+            // Shrink slowly: If the max_element is < the
+            // current size limit, use a limit that is
+            // 90% of the way from max_element to the
+            // current size limit.
+            return (txnsExpected_ * 9 + *iter) / 10;
+        }();
         // Ledgers are processing in a timely manner,
         // so keep the limit high, but don't let it
         // grow without bound.
-        txnsExpected_ = maximumTxnCount_ ?
-            std::min(feeLevels.size(), *maximumTxnCount_) :
-            feeLevels.size();
+        txnsExpected_ = std::min(next,
+            maximumTxnCount_.value_or(next));
     }
 
     JLOG(j_.debug()) << "Escalation multiplication disabled";
@@ -1056,7 +1073,7 @@ TxQ::apply(Application& app, OpenView& view,
             JLOG(j_.warn()) <<
                 "Removing last item of account " <<
                 lastRIter->account <<
-                "from queue with average fee of" <<
+                " from queue with average fee of " <<
                 endEffectiveFeeLevel << " in favor of " <<
                 transactionID << " with fee of " <<
                 feeLevelPaid;
