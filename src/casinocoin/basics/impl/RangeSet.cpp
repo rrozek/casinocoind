f44ec39eca1703725555bfb8ee451edf2d175729
Andre Jochems <a.jochems@cryptoservice.nl>
2019-01-10 08:31:42 -0500

    Handle ledger gaps dynamically

--- src/casinocoin/basics/impl/RangeSet.cpp
+++ src/casinocoin/basics/impl/RangeSet.cpp
@@ -104,7 +104,7 @@ std::uint32_t RangeSet::getPrev (std::uint32_t v) const
 
 // Return the largest number not in the set that is less than the given number
 //
-std::uint32_t RangeSet::prevMissing (std::uint32_t v) const
+std::uint32_t RangeSet::prevMissing (std::uint32_t v, beast::Journal journal) const
 {
     std::uint32_t result = absent;
 
@@ -115,21 +115,25 @@ std::uint32_t RangeSet::prevMissing (std::uint32_t v) const
         // Handle the case where the loop reaches the terminating condition
         //
         result = v - 1;
-        
-// std::clog << "Result 1: " << result << " v: " << v << std::endl;
+
         for (const_reverse_iterator cur = mRanges.rbegin (); cur != mRanges.rend (); ++cur)
         {
             // See if v-1 is in the range
             if (contains (*cur, result))
             {
                 result = cur->first - 1;
-                // handle quorum gap between 154937 and 154940
-                if((result > 154936) && (result < 154941))
-                    result = 154936;
+                // handle any gap in the range
+                if(mLostLedgers.size() > 0)
+                {
+                    while (std::find(mLostLedgers.begin(), mLostLedgers.end(), result) != mLostLedgers.end())
+                    {
+                        JLOG (journal.trace()) << "RangeSet skip missing ledger: " << result;
+                        result = result - 1;
+                    }
+                }
             }
-        }        
+        }
     }
-
     assert (result == absent || !hasValue (result));
 
     return result;
@@ -274,6 +278,16 @@ void RangeSet::checkInternalConsistency () const noexcept
         assert (iter->first <= iter->second);
     }
 #endif
+}
+
+void RangeSet::addLostLedger(std::uint32_t seq, beast::Journal journal)
+{
+    JLOG (journal.trace()) << "RangeSet addLostLedger: " << seq;
+    if (std::find(mLostLedgers.begin(), mLostLedgers.end(), seq) == mLostLedgers.end()) 
+    {
+        // sequence not in vector, add it
+        mLostLedgers.push_back(seq);
+    }
 }
 
 } // casinocoin
