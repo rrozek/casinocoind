b6e9bdbdff2a6effcf080631af1fbbd7564df0da
ReddyArunreddy <arunreddyfulla@gmail.com>
2019-03-26 12:40:36 +0530

    Account for minimum reserve in potential spend:
    * Relevant when deciding whether an account can queue multiple
      transactions. If the potential spend of the already queued
      transactions would dip into the reserve, the reserve is
      preserved for fees.
    * Also change several direct modifications of the owner count to
      call adjustOwnerCount to preserve overflow checking.
    * Update related unit testcase

--- src/casinocoin/app/tx/impl/Escrow.cpp
+++ src/casinocoin/app/tx/impl/Escrow.cpp
@@ -266,7 +266,7 @@ EscrowCreate::doApply()
 
     // Deduct owner's balance, increment owner count
     (*sle)[sfBalance] = (*sle)[sfBalance] - ctx_.tx[sfAmount];
-    (*sle)[sfOwnerCount] = (*sle)[sfOwnerCount] + 1;
+    adjustOwnerCount(ctx_.view(), sle, 1, ctx_.journal);
     ctx_.view().update(sle);
 
     return tesSUCCESS;
@@ -500,7 +500,7 @@ EscrowFinish::doApply()
     // Adjust source owner count
     auto const sle = ctx_.view().peek(
         keylet::account(account));
-    (*sle)[sfOwnerCount] = (*sle)[sfOwnerCount] - 1;
+    adjustOwnerCount(ctx_.view(), sle, -1, ctx_.journal);
     ctx_.view().update(sle);
 
     // Remove escrow from ledger
@@ -583,7 +583,7 @@ EscrowCancel::doApply()
     auto const sle = ctx_.view().peek(
         keylet::account(account));
     (*sle)[sfBalance] = (*sle)[sfBalance] + (*slep)[sfAmount];
-    (*sle)[sfOwnerCount] = (*sle)[sfOwnerCount] - 1;
+    adjustOwnerCount(ctx_.view(), sle, -1, ctx_.journal);
     ctx_.view().update(sle);
 
     // Remove escrow from ledger
