f94b2bf35fe2d75dc1167c839f0148cf66bf10f2
ReddyArunreddy <arunreddyfulla@gmail.com>
2019-05-30 16:11:37 +0530

    Improve crawl shard resource usage

--- src/casinocoin/nodestore/DatabaseShard.h
+++ src/casinocoin/nodestore/DatabaseShard.h
@@ -194,6 +194,14 @@ public:
     static constexpr std::uint32_t ledgersPerShardDefault {16384u};
 };
 
+constexpr
+std::uint32_t
+seqToShardIndex(std::uint32_t seq,
+    std::uint32_t ledgersPerShard = DatabaseShard::ledgersPerShardDefault)
+{
+    return (seq - 1) / ledgersPerShard;
+}
+
 }
 }
 
